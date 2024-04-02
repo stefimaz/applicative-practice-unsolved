69fe5de8d0905142f28a36943d7bfa9d31d626d2
Stephane Masyn <stefimaz@gmail.com>
2024-04-02 17:21:08 -0400

    exe5 done

--- exercises/e5.js
+++ exercises/e5.js
@@ -6,6 +6,13 @@ import { data } from "../data/data";
 
 export function getPlanetsWithMassValue(data, number) {
   // Your code goes here...
+  const planets = data.planets.filter(function(planet) {
+    return planet.mass.massValue >= number;
+   })
+   .map(function(planet) {
+    return planet.name;
+   });
+  return planets; 
 }
 
 
