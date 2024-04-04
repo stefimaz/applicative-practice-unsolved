a29c9c18b356971bb6c6f1f64b502b36a5802dc5
Stephane Masyn <stefimaz@gmail.com>
2024-04-04 16:29:39 -0400

    exe 11 done

--- exercises/e11.js
+++ exercises/e11.js
@@ -7,6 +7,10 @@ import { data } from "../data/data";
 
 export function lowMoonsPlanets(data) {
   // Your code goes here...
+  const planets = data.planets
+    .filter(planet => planet.moons === undefined || planet.moonsCount < 10)
+    .map(planet => planet.name);
+    return planets; 
 }
 
 // === TEST YOURSELF ===
