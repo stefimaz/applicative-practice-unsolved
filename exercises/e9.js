bac9b28004299b6b5bce557a285491f18f67d814
Stephane Masyn <stefimaz@gmail.com>
2024-04-04 15:46:52 -0400

    exe 9 done

--- exercises/e9.js
+++ exercises/e9.js
@@ -6,6 +6,8 @@ import { data } from "../data/data";
 
 export function getEarthData(data) {
   // Your code goes here...
+  const earth = data.planets.find(planet => planet.name === "Earth");
+  return earth;
 }
 
 
