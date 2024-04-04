6a653fd2b5aee5e927e715031b9c542639eaca70
Stephane Masyn <stefimaz@gmail.com>
2024-04-04 15:59:24 -0400

    exe 10 done

--- exercises/e10.js
+++ exercises/e10.js
@@ -6,6 +6,8 @@ import { data } from "../data/data";
 
 export function getAsteroidDataByName(data, asteroidName) {
   // Your code goes here...
+  const asteroid = data.asteroids.find(asteroid => asteroid.name === asteroidName);
+  return asteroid;
 }
 
 
