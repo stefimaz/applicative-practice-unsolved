38fc399e115cdc279cad5a057c118d92872894d8
Stephane Masyn <stefimaz@gmail.com>
2024-04-04 14:28:55 -0400

    exe 6 done

--- exercises/e6.js
+++ exercises/e6.js
@@ -6,6 +6,14 @@ import { data } from "../data/data";
 
 export function getAsteroidsDiscoveredAfterYear(data, year) {
   // Your code goes here...
+  const asteroids = data.asteroids.filter(function(asteroid) {
+    return asteroid.discoveryYear > year;
+   })
+   .map(function(asteroid) {
+    return asteroid.name;
+   });
+   console.log(data);
+  return asteroids; 
 }
 
 
